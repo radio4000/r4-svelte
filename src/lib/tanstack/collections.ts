bedaaea37d2b836ad41a3c798bc88d658613d03b
oskarrough <oskar@rough.dk>
2026-01-26 19:28:08 +0100

    Remove confusing, extra  index file

--- src/lib/tanstack/collections.ts
+++ src/lib/tanstack/collections.ts
@@ -1,3 +1,45 @@
-// Re-export everything from the collections directory
-// This maintains backward compatibility with existing imports
-export * from './collections/index'
+// Re-export all collections and their associated functions
+
+// Shared utilities
+export {queryClient} from './collections/query-client'
+export {log, txLog, offlineLog, getErrorMessage} from './collections/utils'
+
+// Local-only collections (localStorage)
+export {trackMetaCollection, deleteTrackMeta, type TrackMeta} from './collections/track-meta'
+export {
+	playHistoryCollection,
+	addPlayHistoryEntry,
+	endPlayHistoryEntry,
+	clearPlayHistory,
+	type PlayHistoryEntry
+} from './collections/play-history'
+export {spamDecisionsCollection, type SpamDecision} from './collections/spam-decisions'
+
+// Synced collections (with offline support)
+export {followsCollection, followChannel, unfollowChannel, loadUserFollows} from './collections/follows'
+export {
+	channelsCollection,
+	channelsAPI,
+	createChannel,
+	updateChannel,
+	deleteChannel,
+	type Channel
+} from './collections/channels'
+export {broadcastsCollection, readBroadcasts, readBroadcast} from './collections/broadcasts'
+export {
+	tracksCollection,
+	tracksAPI,
+	getTrackWithMeta,
+	addTrack,
+	updateTrack,
+	deleteTrack,
+	batchUpdateTracksUniform,
+	batchUpdateTracksIndividual,
+	batchDeleteTracks,
+	checkTracksFreshness,
+	ensureTracksLoaded,
+	insertDurationFromMeta
+} from './collections/tracks'
+
+// Offline executor
+export {getOfflineExecutor} from './collections/offline-executor'
