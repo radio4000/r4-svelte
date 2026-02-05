376e4edb2769af77066f1db3733a1d3d7055977f
oskarrough <oskar@rough.dk>
2026-02-05 19:09:34 +0100

    Add /chat (unlinked)

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import LiveChat from '$lib/components/live-chat.svelte'
+</script>
+
+<svelte:head>
+	<title>Chat</title>
+</svelte:head>
+
+<div class="constrained container">
+	<LiveChat />
+</div>
