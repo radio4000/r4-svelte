bb5f63b91b4717cf0c686f0d9d4d4ebc5e8e19a7
4www <4www@duck.com>
2026-02-08 17:41:08 +0100

    fix channel card padding

--- src/lib/components/channel-card.svelte
+++ src/lib/components/channel-card.svelte
@@ -58,14 +58,12 @@
 		display: flex;
 		flex-flow: column nowrap;
 		gap: 0.5rem;
-		padding: 0.5rem;
 		text-decoration: none;
 
 		:global(.list) & {
 			display: grid;
 			grid-template-columns: 4rem auto;
 			align-items: center;
-			padding: 0.5rem;
 		}
 
 		:global(.grid) & {
